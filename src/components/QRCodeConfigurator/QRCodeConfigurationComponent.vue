7c68cabe3fc69df06505da4f186aaa003e6b1003
BrentVanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-05-17 17:00:04 +0200

    Build github actions

--- src/components/QRCodeConfigurator/QRCodeConfigurationComponent.vue
+++ src/components/QRCodeConfigurator/QRCodeConfigurationComponent.vue
@@ -1,17 +1,5 @@
-<template>
-	<div class="align-center text-center align-middle QR__code_configuration__component">
-		<div class="QR__code_configuration__component__content">
-			<h1 class="mb-3">Created QR code</h1>
-			<img src="https://picsum.photos/300/300" class="mb-3 ml-auto mr-auto" alt="generated_QR_Code" />
-			<button class="bg-appMainColor text-white font-bold py-2 px-4 rounded inline-flex items-center">
-				<svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-					<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-				</svg>
-				<span>Download</span>
-			</button>
-		</div>
-	</div>
-</template>
+µ
+
 
 <script lang="ts">
 	import { Component, Prop, Vue } from "vue-property-decorator";
