13afa711726f7dc0194971579e35849d1aae29f8
BrentVanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-05-22 21:22:03 +0200

    Init python script for developping qr code generator

---
+++ src/components/QRCodeViewer/SubConfigs/QRCodeFrameConfigurator.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class>
+		<p>QR Code configurator</p>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Prop, Vue } from "vue-property-decorator";
+
+	@Component
+	export default class QRCodeConfiguration extends Vue {
+		@Prop() private msg!: string;
+	}
+</script>
+
+<style lang="scss" scoped>
+	.QR__code_configuration__component {
+		position: relative;
+		.QR__code_configuration__component__content {
+			position: relative;
+			transform: translateY(-50%);
+			top: 50%;
+		}
+	}
+</style>
